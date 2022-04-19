448e5759f834a394a470168fd084d8bd6bf5b178
jaiminbd13 <98934222+jaiminbd13@users.noreply.github.com>
2022-04-19 15:11:54 -0400

    Update WordleDictionary.py

--- WordleDictionary.py
+++ WordleDictionary.py
@@ -1,16 +1,18 @@
+# importing the module
+import os
+
 dictionary_File = "words.txt"
-Word_Length = 5
-
-
 def read_words(word_length=Word_Length) -> str:
     """
-
      :rtype: object
      """
     file = open(dictionary_File, "r")
-    dictionary_list = []
-    for word in file:
-        word = word.strip()
-        if len(word) == Word_Length:
-            dictionary_list.append(word)
-    return dictionary_list
+    filename = 'words.txt'
+    try:
+        with open(filename) as f_obj:
+            contents = f_obj.read()
+    except FileNotFoundError:
+        msg = "Sorry, the file " + filename + "does not exist."
+        print(msg)  # Sorry, the file words.txt does not exist.
+    return dictionary_File
+
