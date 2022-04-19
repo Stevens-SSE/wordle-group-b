f2b2b917c113a9bc7434a0fda541d7e9d5bce56d
jaiminbd13 <98934222+jaiminbd13@users.noreply.github.com>
2022-04-19 15:13:15 -0400

    Update WordleDictionary.py

--- WordleDictionary.py
+++ WordleDictionary.py
@@ -9,10 +9,11 @@ def read_words(word_length=Word_Length) -> str:
     file = open(dictionary_File, "r")
     filename = 'words.txt'
     try:
-        with open(filename) as f_obj:
+        with open(dictionary_File) as f_obj:
             contents = f_obj.read()
     except FileNotFoundError:
-        msg = "Sorry, the file " + filename + "does not exist."
+        msg = "Sorry, the file " + dictionary_File + "does not exist."
         print(msg)  # Sorry, the file words.txt does not exist.
     return dictionary_File
+
 
